d81ca79e46b47a9f1945763cbd5e832656a484d9
Adam <trojnaradam@gmail.com>
2015-07-30 23:03:41 +0200

    utworzenie struktury dokumentu PrimaFaces + Spring + Hibernate + PostgreSQL

---
+++ j2fAPP/src/main/java/model/Customer.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by Connectis2015 on 2015-07-30.
+ */
+public class Customer {
+}
---
+++ j2fAPP/src/main/java/service/CustomerService.java
@@ -0,0 +1,7 @@
+package service;
+
+/**
+ * Created by Connectis2015 on 2015-07-30.
+ */
+public class CustomerService {
+}
