b71cf021d357e1fabe5380cd116a98914f2c8d13
Adam <trojnaradam@gmail.com>
2015-08-02 16:18:03 +0200

    created :pattern" for Dhe model classes (first of entieties)

--- j2fAPP/src/main/java/model/Customer.java
+++ j2fAPP/src/main/java/model/Customer.java
@@ -1,7 +1,50 @@
 package model;
 
-/**
- * Created by Connectis2015 on 2015-07-30.
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CUSTOMER")
 public class Customer {
+    @Id
+    @Column(name = "ID", unique = true, nullable = false)
+    private int id;
+    @Column(name="NAME", unique = true, nullable = false)
+    private String name;
+    @Column(name = "SURNAME", unique = true, nullable = false)
+    private String surname;
+
+    public String toString(){
+        StringBuffer strBuff = new StringBuffer();
+        strBuff.append("id: ").append(getId());
+        strBuff.append(", name: ").append(getName());
+        strBuff.append(", surname: ").append(getSurname());
+        return strBuff.toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
