4ab314bf98ca1d1a78b6ec4dc4328356920ed28d
Adam <trojnaradam@gmail.com>
2015-08-05 23:22:50 +0200

    start create class for Customer Managed Bean

---
+++ j2fAPP/src/main/java/beans/CustomerManagedBean.java
@@ -0,0 +1,10 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.io.Serializable;
+
+@ManagedBean(name="customerMB")
+@RequestScoped
+public class CustomerManagedBean implements Serializable {
+}
